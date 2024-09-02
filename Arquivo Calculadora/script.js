cff06672b5c2a6105cea86b8fb6efe630bbf570f
caiopalmardemelo <159266607+caiopalmardemelo@users.noreply.github.com>
2024-09-02 09:39:06 -0300

    Add files via upload

---
+++ Arquivo Calculadora/script.js
@@ -0,0 +1,20 @@
+alert("oioi ola");
+
+function calculadora(opera) {
+    let num1 = parseFloat.getElementById('iem').value;
+    let num2 = parseFloat.getElementById('idem').value;
+    document.getElementById('resultado')textContent = resultado
+
+    if(isNaN(num1) || isNaN(num2))
+        alert('-')
+}
+
+if (opera == '+'){
+    resultado = num1+num2;
+}else if (opera == '-'){
+    resultado = num1-num2;
+}else if (opera == '*'){
+    resultado = num1*num2;
+}else if (opera == '/'){
+    resultado = num1/num2;
+}
